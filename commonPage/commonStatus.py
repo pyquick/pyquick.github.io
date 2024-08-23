167265cc65f345c47a12f3fbe1355a2e674ffe4c
githubtohaoyangli <fairzlq@hotmail.com>
2024-08-23 20:49:48 +0800

    change

--- commonPage/commonStatus.py
+++
@@ -1,3 +0,0 @@
-from django.http import JsonResponse, HttpResponse
-def http404(request,exception=None):
-    return HttpResponse(request,"404.html",status=404)
--- run.pyw
+++
@@ -1,3 +0,0 @@
-#python manage.py runserver 0.0.0.0:8000
-import os
-os.system("python3 manage.py runserver 0.0.0.0:8000")
